e17255e19a5f969193598c835bd0b2ec167f724a
Kenya Chan <kenya.chan@gmail.com>
2025-06-12 11:38:09 +1000

    Add about taskpane

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -6,31 +6,15 @@
 /* global console, document, Excel, Office */
 
 Office.onReady((info) => {
-  if (info.host === Office.HostType.Excel) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-  }
+ 	if (info.host === Office.HostType.Excel) {
+		document.querySelector('.author-text>a').addEventListener('click', openLink);
+		document.getElementById("app-body").style.display = "flex";
+	}
 });
 
-export async function run() {
-  try {
-    await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
+function openLink() {
+	const url = 'https://github.com/kenyachan/';
 
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
+	window.open(url);
 }
+
