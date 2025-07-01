36a5dbb481bca2f67f3e23044eb3c9c50668be1f
Kenya Chan <kenya.chan@gmail.com>
2025-07-01 14:48:45 +1000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e	# Exit on error
 
 # Change branch and sync
-git checkout dist && git merge main --no-edit
+git checkout deploy && git merge main --no-edit
 
 # Commit the build folder if it has changes
 git add dist -f
@@ -12,4 +12,7 @@ git commit -m "Build for deploy" || echo "No changes to commit"
 npm run build
 
 # Push the dist folder as a subtree to the dist branch
-git subtree push --prefix dist origin dist
+git subtree push --prefix dist origin deploy
+
+# Change back to main branch
+git checkout main
