4d4c700c227f1b29bcc16f0891e2e61af8ba9c6e
Kenya Chan <kenya.chan@gmail.com>
2025-07-01 14:51:52 +1000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ set -e	# Exit on error
 # Change branch and sync
 git checkout deploy && git merge main --no-edit
 
+# Ensure latest build
+npm run build
+
 # Commit the build folder if it has changes
 git add dist -f
 git commit -m "Build for deploy" || echo "No changes to commit"
-
-# Ensure latest build
-npm run build
 
 # Push the dist folder as a subtree to the dist branch
 git subtree push --prefix dist origin deploy
